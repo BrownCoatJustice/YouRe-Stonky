c827b783717f199895d6b7142cdbd4c9bbdb19b6
CreepTheCat <brownforsocialmedia@gmail.com>
2023-07-22 20:43:10 +0530

    dont mind me just some more testing

--- src/main/java/habis/App.java
+++ src/main/java/habis/App.java
@@ -5,7 +5,6 @@ package habis;
  */
 public final class App {
     public static void main(String[] args) {
-        System.out.println("");
         System.out.println("Your grandpa left you a generous $10,000 in his will. \nUse it wisely in the stock market: where fortunes soar and plummet like a roller coaster on a caffeine binge.");
         System.out.println("Want help? type 'help' in the prompt box.\nTo start, play on a scenario or use the default scenario!");
     }
